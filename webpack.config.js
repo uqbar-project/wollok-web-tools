baf3dc85defe88f7b71f8542e22ccfc31fb0bf4e
palumbon <nahuel.palumbo@gmail.com>
2024-07-01 22:25:08 +0200

    Build as library and web

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
   },
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist', 'web'),
   },
   watchOptions: {
     ignored: "/node_modules/",
