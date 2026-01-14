595ed024872afb581612296f82adbb327eaf5273
ivojawer <ivojawerbaum@gmail.com>
2026-01-13 21:25:36 -0300

    export socketgame

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,4 @@ export * from './dynamicDiagram/diagram-generator.js'
 // Game
 export * from './game/utils.js'
 
-export { LocalGame } from './game/game.js'
+export { LocalGame, SocketGame } from './game/game.js'
